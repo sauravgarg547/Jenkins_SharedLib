7d20c91af14b5599faf237d7afca5de140151afb
Saurav Garg <72258364+sauravgarg547@users.noreply.github.com>
2025-07-14 14:39:13 +0530

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,12 @@
-def call(){
-  timeout(time: 1, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: false
-  }
+def call() {
+    timeout(time: 1, unit: "MINUTES") {
+        def qualityGate = waitForQualityGate(abortPipeline: false)
+        echo "✅ SonarQube Quality Gate Status: ${qualityGate.status}"
+
+        // Save the result into a file
+        writeFile file: 'sonar-quality-gate.txt', text: qualityGate.status
+
+        // Export to env variable so it can be used in email
+        env.QUALITY_GATE = qualityGate.status
+    }
 }
